bed657339179b4655792640c7f6778f28abb06d7
AratiGadgil <45192341+AratiGadgil@users.noreply.github.com>
2023-12-26 06:15:30 +0000

    Datatypes

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+console.log("Hello");
+let accountId = 1234566
+let accounType = "User"
+
+console.table([accountId,accounType])
+let temp = "temp"
+console.log(typeof temp)
+temp = null
+console.log(typeof temp)
